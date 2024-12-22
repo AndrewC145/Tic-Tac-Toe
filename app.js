24e17639064bc24a1329a1b54d5e69b13768f3cc
Andrew Cao <andrewcao618@gmail.com>
2024-12-22 15:02:22 -0500

    Fixed Bug for ResetBtn not showing Marks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const twoPlayerBtn = document.querySelector(".two-players-btn");
 const computerBtn = document.querySelector(".computer-btn");
+const message = document.querySelector(".message");
 const resetBtn = document.querySelector(".reset");
 const playAgainBtn = document.querySelector(".play-again");
 const boardCell = document.querySelectorAll(".cell");
@@ -29,6 +30,12 @@ const players = {
   }
 }
 
+function createGame() {
+  gameBoard.board = ['', '', '', '', '', '', '', '', ''];
+  boardCell.forEach(cell => cell.textContent = '');
+  gameFinish = false;
+}
+
 function playGame() {
   let currentPlayer = players.player1;
   let gameFinish = false;
@@ -42,8 +49,18 @@ function playGame() {
           currentPlayer.score++;
           scoreUpdate();
           gameFinish = true;
-          
+        } else if (gameBoard.board.every(cell => cell !== '')) {
+          alert("It's a draw!");
+          gameFinish = true;
+        } else {
+          currentPlayer = currentPlayer === players.player1 ? players.player2 : players.player1;
         }
+      if (currentPlayer === players.player1) {
+        message.textContent = `${players.player1.name}'s turn`;
+      }
+      else if (currentPlayer === players.player2) {
+        message.textContent = `${players.player2.name}'s turn`;
+      }
       }
     });
   });
@@ -71,6 +88,19 @@ function scoreUpdate() {
   const player1Score = document.querySelector(".player-one-score");
   const player2Score = document.querySelector(".player-two-score");
 
-  player1Score.textContent = `Player 1: ${players.player1.score}`;
-  player2Score.textContent = `Player 2: ${players.player2.score}`; 
+  player1Score.textContent = `Player 1 Score: ${players.player1.score}`;
+  player2Score.textContent = `Player 2 Score: ${players.player2.score}`; 
 }
+
+resetBtn.addEventListener("click", () => {
+  createGame();
+  players.player1.score = 0;
+  players.player2.score = 0;
+  scoreUpdate();
+  playGame();
+});
+
+playAgainBtn.addEventListener("click", () => {
+  createGame();
+  scoreUpdate();
+});
